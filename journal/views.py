9ffac1046a1a0c29aaf93e0d349c35514c29436d
adawg2010 <jcebersole1@gmail.com>
2025-04-24 20:05:38 +0900

    8pm break

--- journal/views.py
+++ journal/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+from .model import Topic,Entry
 
 def home(request):
     return render(request, 'index.html')
+
