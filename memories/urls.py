131d68a319c60db1a60cdae365664b16007d5523
adawg2010 <jcebersole1@gmail.com>
2025-04-24 18:55:13 +0900

    logic moves

--- memories/urls.py
+++ memories/urls.py
@@ -19,5 +19,6 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('journal.urls')),
       path("__reload__/", include("django_browser_reload.urls")),
 ]
